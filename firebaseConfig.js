516c1ee97b7010ad5137d80f49f238401372514a
Eazytutors <etopspune@gmail.com>
2025-06-15 14:25:37 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyB2WcCWwPUp3nVD6YQMKszvb9vIOvS0TjM",
+  authDomain: "eazyportal-b5dc5.firebaseapp.com",
+  projectId: "eazyportal-b5dc5",
+  storageBucket: "eazyportal-b5dc5.appspot.com",
+  messagingSenderId: "237694647038",
+  appId: "1:237694647038:web:f77b0c93b89cf29cb003af"
+};
